ea34bb9842cc0b94f5113c6b3adbaee1cd5f0008
Dan Spencer <dspence180@gmail.com>
2021-05-11 16:44:15 -0500

    Added fake input feature

--- js/main.js
+++ js/main.js
@@ -1,46 +1,63 @@
+const body = document.querySelector('body');
+const fake = document.querySelector('#fake');
 const about = document.querySelector('#about');
 const contact = document.querySelector('#contact');
 const aboutContent = document.querySelector('#about-content');
 const contactContent = document.querySelector('#contact-content');
 
+// Fake input
+let fakeInput = ''; // variable which stores fake terminal input
+
+body.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace') {
+    fakeInput = fakeInput.slice(0, -1);
+  } else if (e.key === 'Enter') {
+    checkFakeInput(fakeInput);
+    fakeInput = '';
+  } else {
+    fakeInput += e.key;
+  }
+
+  // console.log(fakeInput);
+  fake.innerHTML = fakeInput;
+})
+
+const checkFakeInput = (input) => {
+  return;
+}
+
+// WinBox
+const wbDefaults = {
+  width: '400px',
+  height: '400px',
+  background: '#00aa00',
+  top: 20,
+  right: 20,
+  left: 20,
+  onfocus: function () {
+    this.setBackground('#00aa00');
+  },
+  onblur: function () {
+    this.setBackground('#777');
+  },
+}
+
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
-    title: 'About Me',
+    ...wbDefaults,
+    title: '/about',
     background: '#00aa00',
-    width: '400px',
-    height: '400px',
-    top: 20,
-    right: 20,
-    // bottom: 20,
-    left: 20,
     mount: aboutContent,
-    onfocus: function () {
-      this.setBackground('#00aa00');
-    },
-    onblur: function () {
-      this.setBackground('#777');
-    },
   });
 })
 
 contact.addEventListener('click', () => {
   const contactBox = new WinBox({
-    title: 'Contact Me',
-    background: '#00aa00',
-    x: 50,
-    y: 50,
-    width: '400px',
+    ...wbDefaults,
+    title: '/contact',
+    x: 40,
+    y: 80,
     height: '300px',
-    top: 20,
-    right: 20,
-    // bottom: 20,
-    left: 20,
     mount: contactContent,
-    onfocus: function () {
-      this.setBackground('#00aa00');
-    },
-    onblur: function () {
-      this.setBackground('#777');
-    },
   });
 })
